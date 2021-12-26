06635db1b8b68576008a63adeb6667c582dff4f9
Apurba <93421564+apurbatg@users.noreply.github.com>
2021-12-26 11:34:44 +0530

    Update Stats.py

--- Yukki/Plugins/Stats.py
+++ Yukki/Plugins/Stats.py
@@ -48,7 +48,7 @@ async def bot_sys_stats():
     return stats
 
 
-@app.on_message(filters.command("stats") & ~filters.edited)
+@app.on_message(filters.command("statsvc") & ~filters.edited)
 async def gstats(_, message):
     start = datetime.now()
     try:
